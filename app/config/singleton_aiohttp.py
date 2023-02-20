a46dfc33d22b737d9dc85e6599f8121814c70151
toniPortillo <toniplsec@gmail.com>
2023-02-20 19:23:50 +0100

    Adding singleton for aio http client

--- app/config/singleton_aiohttp.py
+++ app/config/singleton_aiohttp.py
@@ -2,8 +2,6 @@ import asyncio
 from socket import AF_INET
 from typing import Optional
 
-from fastapi import APIRouter
-
 import aiohttp
 
 SIZE_POOL_AIOHTTP = 100
@@ -16,4 +14,18 @@ class SingletonAiohttp:
     @classmethod
     def get_aiohttp_client(cls) -> aiohttp.ClientSession:
         if cls.aiohttp_client is None:
-            timeout = aiohttp.Clien
+            timeout = aiohttp.ClientTimeout(total=2)
+            connector = aiohttp.TCPConnector(
+                family=AF_INET, limit_per_host=SIZE_POOL_AIOHTTP, ssl=False
+            )
+            cls.aiohttp_client = aiohttp.ClientSession(
+                timeout=timeout, connector=connector
+            )
+
+        return cls.aiohttp_client
+
+    @classmethod
+    async def close_aiohttp_client(cls) -> None:
+        if cls.aiohttp_client:
+            await cls.aiohttp_client.close()
+            cls.aiohttp_client = None
