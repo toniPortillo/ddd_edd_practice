c8f64e2f929417d1a781d2d241992b5df37bf1a6
toniPortillo <toniplsec@gmail.com>
2023-04-27 18:17:40 +0200

    Another step towards for the integration testing infrastructure

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -1,8 +1,17 @@
 import pytest
 from fastapi.testclient import TestClient
+from dependency_injector import providers
 
 from app.ddd_edd_practice_app import app
+from test.integration.config.database_container import DatabaseContainer
 
+
+@pytest.fixture(scope="session", autouse=True)
+def test_db_configuration():
+    database_container = providers.Container(
+        DatabaseContainer,
+    )
+    app.container[0].override(database_container)
 
 @pytest.fixture(scope="session")
 def test_client():
