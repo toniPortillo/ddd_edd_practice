883238a0939825960e616d411e3d217e690a5bad
toniPortillo <toniplsec@gmail.com>
2023-05-18 20:32:33 +0200

    Refactoring db infrastructure its also neeeded change integration tests infrastructure

--- src/stock/infrastructure/sqlalchemy_stock_repository.py
+++ src/stock/infrastructure/sqlalchemy_stock_repository.py
@@ -5,7 +5,6 @@ from src.stock.domain.stock import Stock
 from src.stock.domain.stock_repository import StockRepository
 
 from sqlalchemy.orm import Session, Query
-from src.stock.infrastructure.persistence.stock_mapper import StockMapper
 
 
 class SqlalchemyStockRepository(StockRepository):
@@ -14,73 +13,25 @@ class SqlalchemyStockRepository(StockRepository):
 
     async def find_all(self) -> List[Stock]:
         with self.__db_instance() as session:
-            query: Query = session.query(StockMapper)
-            db_stocks: List[StockMapper] = query.all()
-            stocks: List[Stock] = []
-            for db_stock in db_stocks:
-                stocks.append(
-                    Stock(
-                        stock_id=UUID(db_stock.id),
-                        symbol=db_stock.symbol,
-                        name=db_stock.name,
-                        currency=db_stock.currency,
-                        exchange=db_stock.exchange,
-                        mic_code=db_stock.mic_code,
-                        country=db_stock.country,
-                        type=db_stock.type,
-                    )
-                )
-        return stocks
+            query: Query = session.query(Stock)
+            stocks: List[Stock] = query.all()
+
+            return stocks
 
     async def find_by_id(self, id: UUID) -> Optional[Stock]:
         with self.__db_instance() as session:
-            db_stock: Optional[StockMapper] = session.query(StockMapper).get(str(id))
-            if db_stock is not None:
-                stock: Stock = Stock(
-                    stock_id=UUID(db_stock.id),
-                    symbol=db_stock.symbol,
-                    name=db_stock.name,
-                    currency=db_stock.currency,
-                    exchange=db_stock.exchange,
-                    mic_code=db_stock.mic_code,
-                    country=db_stock.country,
-                    type=db_stock.type,
-                )
+            stock: Optional[Stock] = session.query(Stock).get(str(id))
 
-        return stock
+            return stock
 
     async def save(self, stock: Stock) -> None:
-        stock_mapper: StockMapper = StockMapper(
-            id=str(stock.stock_id),
-            symbol=str(stock.symbol),
-            name=str(stock.name),
-            currency=str(stock.currency),
-            exchange=str(stock.exchange),
-            mic_code=str(stock.mic_code),
-            country=str(stock.country),
-            type=str(stock.type),
-        )
         with self.__db_instance() as session:
-            session.add(stock_mapper)
+            session.add(stock)
             session.commit()
-            session.refresh(stock_mapper)
+            session.refresh(stock)
 
     async def save_many(self, stocks: List[Stock]) -> int:
-        stocks_mapped: List[StockMapper] = []
-        for stock in stocks:
-            stock_mapper: StockMapper = StockMapper(
-                id=str(stock.stock_id),
-                symbol=str(stock.symbol),
-                name=str(stock.name),
-                currency=str(stock.currency),
-                exchange=str(stock.exchange),
-                mic_code=str(stock.mic_code),
-                country=str(stock.country),
-                type=str(stock.type),
-            )
-            stocks_mapped.append(stock_mapper)
-
         with self.__db_instance() as session:
-            session.add_all(stocks_mapped)
+            session.add_all(stocks)
             session.commit()
-            return len(stocks_mapped)
+            return len(stocks)
--- test/unit/stock/infrastructure/test_sqlalchemy_stock_repository.py
+++ test/unit/stock/infrastructure/test_sqlalchemy_stock_repository.py
@@ -34,36 +34,22 @@ class TestSqlAlchemyStockRepository:
             },
         ]
 
-        mock_stocks_mappers_result = []
-        for fake_stock in fake_query_result:
-            stock_mapper_mock = Mock()
-            stock_mapper_mock.id = fake_stock["id"]
-            stock_mapper_mock.symbol = fake_stock["symbol"]
-            stock_mapper_mock.name = fake_stock["name"]
-            stock_mapper_mock.currency = fake_stock["currency"]
-            stock_mapper_mock.exchange = fake_stock["exchange"]
-            stock_mapper_mock.mic_code = fake_stock["mic_code"]
-            stock_mapper_mock.country = fake_stock["country"]
-            stock_mapper_mock.type = fake_stock["type"]
-            mock_stocks_mappers_result.append(
-                stock_mapper_mock
-            )
         stocks_fake: List[Stock] = []
-        for stock_mapper_fake in mock_stocks_mappers_result:
+        for stock_mapper_fake in fake_query_result:
             stocks_fake.append(
                 Stock(
-                    stock_id=UUID(stock_mapper_fake.id),
-                    symbol=stock_mapper_fake.symbol,
-                    name=stock_mapper_fake.name,
-                    currency=stock_mapper_fake.currency,
-                    exchange=stock_mapper_fake.exchange,
-                    mic_code=stock_mapper_fake.mic_code,
-                    country=stock_mapper_fake.country,
-                    type=stock_mapper_fake.type,
+                    stock_id=UUID(stock_mapper_fake['id']),
+                    symbol=stock_mapper_fake['symbol'],
+                    name=stock_mapper_fake['name'],
+                    currency=stock_mapper_fake['currency'],
+                    exchange=stock_mapper_fake['exchange'],
+                    mic_code=stock_mapper_fake['mic_code'],
+                    country=stock_mapper_fake['country'],
+                    type=stock_mapper_fake['type'],
                 )
             )
 
-        query_mock.all.return_value = mock_stocks_mappers_result
+        query_mock.all.return_value = stocks_fake
         db_instance_session.session_mock.query.return_value = query_mock
         sqlalchemy_stock_repository = SqlalchemyStockRepository(db_instance=db_instance_session)
         stocks = await sqlalchemy_stock_repository.find_all()
@@ -83,28 +69,18 @@ class TestSqlAlchemyStockRepository:
             "type": "equity",
         }
 
-        stock_mapper_mock = Mock()
-        stock_mapper_mock.id = fake_query_result["id"]
-        stock_mapper_mock.symbol = fake_query_result["symbol"]
-        stock_mapper_mock.name = fake_query_result["name"]
-        stock_mapper_mock.currency = fake_query_result["currency"]
-        stock_mapper_mock.exchange = fake_query_result["exchange"]
-        stock_mapper_mock.mic_code = fake_query_result["mic_code"]
-        stock_mapper_mock.country = fake_query_result["country"]
-        stock_mapper_mock.type = fake_query_result["type"]
-
         stock_fake: Stock = Stock(
-            stock_id=UUID(stock_mapper_mock.id),
-            symbol=stock_mapper_mock.symbol,
-            name=stock_mapper_mock.name,
-            currency=stock_mapper_mock.currency,
-            exchange=stock_mapper_mock.exchange,
-            mic_code=stock_mapper_mock.mic_code,
-            country=stock_mapper_mock.country,
-            type=stock_mapper_mock.type,
+            stock_id=UUID(fake_query_result['id']),
+            symbol=fake_query_result['symbol'],
+            name=fake_query_result['name'],
+            currency=fake_query_result['currency'],
+            exchange=fake_query_result['exchange'],
+            mic_code=fake_query_result['mic_code'],
+            country=fake_query_result['country'],
+            type=fake_query_result['type'],
         )
 
-        query_mock.get.return_value = stock_mapper_mock
+        query_mock.get.return_value = stock_fake
         db_instance_session.session_mock.query.return_value = query_mock
         sqlalchemy_stock_repository = SqlalchemyStockRepository(db_instance=db_instance_session)
         stock = await sqlalchemy_stock_repository.find_by_id(id=stock_fake.stock_id)
@@ -122,24 +98,13 @@ class TestSqlAlchemyStockRepository:
             country="US",
             type="equity",
         )
-        
-        stock_mapper_fake = Mock()
-        stock_mapper_fake.id = str(stock_fake.stock_id)
-        stock_mapper_fake.symbol = stock_fake.symbol
-        stock_mapper_fake.name = stock_fake.name
-        stock_mapper_fake.currency = stock_fake.currency
-        stock_mapper_fake.exchange = stock_fake.exchange
-        stock_mapper_fake.mic_code = stock_fake.mic_code
-        stock_mapper_fake.country = stock_fake.country
-        stock_mapper_fake.type = stock_fake.type
-        mocker.patch('src.stock.infrastructure.sqlalchemy_stock_repository.StockMapper', return_value=stock_mapper_fake)
 
         sqlalchemy_stock_repository = SqlalchemyStockRepository(db_instance=db_instance_session)
         await sqlalchemy_stock_repository.save(stock=stock_fake)
 
-        db_instance_session.session_mock.add.assert_called_once_with(stock_mapper_fake)
+        db_instance_session.session_mock.add.assert_called_once_with(stock_fake)
         db_instance_session.session_mock.commit.assert_called_once()
-        db_instance_session.session_mock.refresh.assert_called_once_with(stock_mapper_fake)
+        db_instance_session.session_mock.refresh.assert_called_once_with(stock_fake)
 
     async def test_save_many(self, mocker, db_instance_session):
         fake_query_result = [
@@ -180,24 +145,9 @@ class TestSqlAlchemyStockRepository:
                 )
             )
 
-        stock_mapper_fakes: List[Mock] = []
-        for fake_result in fake_query_result:
-            stock_mapper_fake = Mock()
-            stock_mapper_fake.id = fake_result["id"]
-            stock_mapper_fake.symbol = fake_result["symbol"]
-            stock_mapper_fake.name = fake_result["name"]
-            stock_mapper_fake.currency = fake_result["currency"]
-            stock_mapper_fake.exchange = fake_result["exchange"]
-            stock_mapper_fake.mic_code = fake_result["mic_code"]
-            stock_mapper_fake.country = fake_result["country"]
-            stock_mapper_fake.type = fake_result["type"]
-            stock_mapper_fakes.append(stock_mapper_fake)
-
-        mocker.patch('src.stock.infrastructure.sqlalchemy_stock_repository.StockMapper', side_effect=stock_mapper_fakes)
-
         sqlalchemy_stock_repository = SqlalchemyStockRepository(db_instance=db_instance_session)
         stocks_saved: int = await sqlalchemy_stock_repository.save_many(stocks=stocks_fake)
 
-        db_instance_session.session_mock.add_all.assert_called_once_with(stock_mapper_fakes)
+        db_instance_session.session_mock.add_all.assert_called_once_with(stocks_fake)
         db_instance_session.session_mock.commit.assert_called_once()
         assert stocks_saved == 2
